d714d51d5e1e61781474112961648dce19442e84
Roger Henrique Diegoli <rogerhenriquediegoli@gmail.com>
2023-10-21 01:20:21 -0300

    update
    Versão 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,52 @@
 var numClicks = 0
-var gridGame = [[0,0,0], [0,0,0], [0,0,0]]
+var status_jogador = document.getElementById('text-status-game')
+var positionsX = []
+var positionsO = []
 
 function clickTd(id){
-    numClicks++
-    console.log(numClicks, id)
-    let fieldElement = document.getElementById(`td${id}`)
-    if(fieldElement.innerHTML == ""){
-        let image = document.createElement('img')
-        if(numClicks % 2 == 0){
-            image.src = 'images/simbol-x.png'
-        }else{
-            image.src = 'images/simbol-o.png'
+    if(numClicks != -1){
+        let fieldElement = document.getElementById(`td${id}`)
+        if(fieldElement.innerHTML == ""){
+            let image = document.createElement('img')
+            if(numClicks % 2 == 0){
+                status_jogador.innerHTML = `Vez do <strong>O</strong>`
+                image.src = 'images/simbol-x.png'
+                positionsX.push(id)
+            }else{
+                image.src = 'images/simbol-o.png'
+                status_jogador.innerHTML = `Vez do <strong>X</strong>`
+                positionsO.push(id)
+            }
+            fieldElement.appendChild(image)
+            numClicks++
+            endGame()
         }
-
-    
-        fieldElement.appendChild(image)
     }
+    
+}
+
+function endGame(){
+    if (positionsX.includes(0) && positionsX.includes(4) && positionsX.includes(8) || positionsX.includes(2) && positionsX.includes(4) && positionsX.includes(6)|| positionsX.includes(0) && positionsX.includes(1) && positionsX.includes(2) || positionsX.includes(6) && positionsX.includes(7) && positionsX.includes(8) || positionsX.includes(0) && positionsX.includes(3) && positionsX.includes(6) || positionsX.includes(2) && positionsX.includes(5) && positionsX.includes(8) || positionsX.includes(1) && positionsX.includes(4) && positionsX.includes(7) || positionsX.includes(3) && positionsX.includes(4) && positionsX.includes(5)){
+        setTimeout(() => {
+            status_jogador.innerHTML = `Jogador <strong>X</strong> foi o vencedor`
+            alert("O jogador X venceu!")
+        }, 10);
+        numClicks = -1
+    }else if(positionsO.includes(0) && positionsO.includes(4) && positionsO.includes(8) || positionsO.includes(2) && positionsO.includes(4) && positionsO.includes(6)|| positionsO.includes(0) && positionsO.includes(1) && positionsO.includes(2) || positionsO.includes(6) && positionsO.includes(7) && positionsO.includes(8) || positionsO.includes(0) && positionsO.includes(3) && positionsO.includes(6) || positionsO.includes(2) && positionsO.includes(5) && positionsO.includes(8) || positionsO.includes(1) && positionsO.includes(4) && positionsO.includes(7) || positionsO.includes(3) && positionsO.includes(4) && positionsO.includes(5)){
+        setTimeout(() => {
+            status_jogador.innerHTML = `Jogador <strong>O</strong> foi o vencedor`
+            alert("O jogador O venceu!")
+        }, 10);
+        numClicks = -1
+    }else if(positionsX.length == 5){
+        setTimeout(() => {
+            status_jogador.innerHTML = `<strong>Empate</strong>`
+            alert("Empate!")
+        }, 10);
+    }   
+}
+
+function reload(){
+    location.reload()
 }
 
